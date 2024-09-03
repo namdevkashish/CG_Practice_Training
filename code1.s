8ab374cb4cd44d37118de3b1777158df027df712
kritika <kritich1811@gmail.com>
2024-09-03 05:36:52 +0000

    Code1.c

---
+++ code1.s
@@ -0,0 +1,37 @@
+	.file	"Code1.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"The sum of %d and %d is %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB5:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$5, -8(%rbp)
+	movl	$6, -4(%rbp)
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	leal	(%rdx,%rax), %ecx
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE5:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
